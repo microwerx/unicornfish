f93ee109d01b7e964fb53af2aad01b6df1ac8462
Jonathan Metzgar <microwerx@outlook.com>
2019-09-12 16:00:19 -0800

    move objstaticmodel class to fluxions

--- include/unicornfish_corona.hpp
+++ include/unicornfish_corona.hpp
@@ -30,65 +30,21 @@
 
 namespace Fluxions
 {
-
-class OBJStaticModel
-{
-  public:
-	struct Vertex
+	class CoronaDatabase
 	{
-		Vector3f position;
-		Vector3f normal;
-		Vector2f texcoord;
-		Vector3f tangent;
-		Vector3f binormal;
+	private:
+	public:
+		SimpleRenderer_GLuint renderer;
+		std::map<std::string, SimpleStaticMesh> models;
+
+		CoronaDatabase();
+		~CoronaDatabase();
+
+		void LoadOBJ(const std::string& objname, const std::string& filename);
+		void BuildBuffers();
+		void Render();
+		void RenderZOnly();
 	};
-	struct Surface
-	{
-		GLenum mode = GL_TRIANGLES;
-		GLint first = 0;
-		GLsizei count = 0;
-		std::string materialName;
-		std::string surfaceName;
-	};
-
-  public:
-	OBJStaticModel();
-	~OBJStaticModel();
-
-	bool LoadOBJ(const std::string &filename);
-	bool SaveOBJ(const std::string &filename);
-	void SavePrecompiled(const std::string &filename, const std::string objname);
-	void LoadPrecompiled(const int numIndices, const int *indices, const int numVertices, const float *vertices, const int numSurfaces, const OBJSurface *surfaces);
-	void ComputeTangentVectors();
-	void Clear();
-	void Resize(int vertexCount, int indexCount, int surfaceCount = 1);
-	void CreateSimpleModel(int vertexCount, int indexCount, int surfaceCount = 1);
-	void Transform(const Matrix4d &mat);
-
-	void Render(SimpleRenderer_GLuint &renderer);
-	void RenderGL11();
-
-	std::vector<Vertex> Vertices;
-	std::vector<GLuint> Indices;
-	std::vector<Surface> Surfaces;
-	BoundingBoxf BoundingBox;
-};
-
-class CoronaDatabase
-{
-  private:
-  public:
-	SimpleRenderer_GLuint renderer;
-	std::map<std::string, OBJStaticModel> models;
-
-	CoronaDatabase();
-	~CoronaDatabase();
-
-	void LoadOBJ(const std::string &objname, const std::string &filename);
-	void BuildBuffers();
-	void Render();
-	void RenderZOnly();
-};
 } // namespace Fluxions
 
 #endif
